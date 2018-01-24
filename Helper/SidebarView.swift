a5efd441e00b8763e02dcaa406c4fa7a26ad897a
Gul Mehru <gulmehru@guls-macbook-air.local>
2018-01-24 17:48:47 +0500

    view controller

---
+++ Helper/SidebarView.swift
@@ -0,0 +1,138 @@
+//
+//  AppDelegate.swift
+//  JetCompare
+//
+//  Created by GulMehru on 20/01/2018.
+//  Copyright © 2018 gul. All rights reserved.
+//
+
+
+
+import Foundation
+import UIKit
+
+protocol SidebarViewDelegate: class {
+    func sidebarDidSelectRow(row: Row)
+}
+
+enum Row: String {
+    case editProfile
+    case home
+    case flights
+    case hotels
+    case cars
+    case insurance
+    case blog
+    case about_us
+    case contact_us
+    case none
+    
+    init(row: Int) {
+        switch row {
+        case 0: self = .editProfile
+        case 1: self = .home
+        case 2: self = .flights
+        case 3: self = .hotels
+        case 4: self = .cars
+        case 5: self = .insurance
+        case 6: self = .blog
+        case 7: self = .about_us
+        case 8: self = .contact_us
+        default: self = .none
+        }
+    }
+}
+
+class SidebarView: UIView, UITableViewDelegate, UITableViewDataSource {
+
+    var titleArr = [String]()
+    
+    weak var delegate: SidebarViewDelegate?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor=UIColor.white
+        //(red: 54/255, green: 55/255, blue: 56/255, alpha: 1.0)
+        self.clipsToBounds=true
+        
+        titleArr = [ " ", "Home", "Flights", "Hotels", "Cars", "Insurance", "Blog", "About us", "Contact us"]
+     
+        setupViews()
+        self.myTableView.rowHeight = 5.0
+        myTableView.delegate=self
+        myTableView.dataSource=self
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        myTableView.estimatedRowHeight = 2
+        myTableView.layer.masksToBounds = false
+        myTableView.layer.borderWidth = 0;
+        myTableView.tableFooterView=UIView()
+        myTableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        //myTableView.allowsSelection = true
+        myTableView.bounces=false
+        myTableView.showsVerticalScrollIndicator=false
+        myTableView.backgroundColor = UIColor.clear
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.selectionStyle = .none
+        if indexPath.row == 0 {
+            //cell.backgroundColor=UIColor(red: 77/255, green: 77/255, blue: 77/255, alpha: 1.0)
+            let cellImg: UIImageView!
+            cellImg = UIImageView(frame: CGRect(x: 10, y: 10, width: 230, height: 45))
+            //cellImg.layer.cornerRadius = 5
+            cellImg.layer.masksToBounds=true
+            //cellImg.contentMode = .scaleAspectFill
+            cellImg.layer.masksToBounds=true
+            cellImg.image=#imageLiteral(resourceName: "logoPayL")
+            cell.addSubview(cellImg)
+            
+            let cellLbl = UILabel(frame: CGRect(x: 110, y: cell.frame.height/2-15, width: 250, height: 120))
+            cell.addSubview(cellLbl)
+            cellLbl.text = titleArr[indexPath.row]
+            cellLbl.font=UIFont.systemFont(ofSize: 17)
+            cellLbl.textColor=UIColor.white
+        } else {
+            cell.textLabel?.text=titleArr[indexPath.row]
+            cell.textLabel?.textColor=UIColor.darkGray
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.delegate?.sidebarDidSelectRow(row: Row(row: indexPath.row))
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 85
+        } else {
+            return 40
+        }
+    }
+    
+    func setupViews() {
+        self.addSubview(myTableView)
+        myTableView.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        myTableView.leftAnchor.constraint(equalTo: leftAnchor).isActive=true
+        myTableView.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
+        myTableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive=true
+    }
+    
+    let myTableView: UITableView = {
+        let table=UITableView()
+        table.translatesAutoresizingMaskIntoConstraints=false
+        return table
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
--- payLessTravel/ViewController.swift
+++ payLessTravel/ViewController.swift
@@ -1,25 +1,156 @@
-//
-//  ViewController.swift
-//  payLessTravel
-//
-//  Created by Gul Mehru on 1/24/18.
-//  Copyright © 2018 Gul Mehru. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    
+    var sidebarView: SidebarView!
+    var blackScreen: UIView!
+    
+    var mainActivityIndicator : UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRect(x:100 ,y:200, width:80, height: 80)) as UIActivityIndicatorView
+    
+    
+    let myWebView : UIWebView = {
+        var wv = UIWebView()
+        wv = UIWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        
+        return wv
+    }()
+    
+    func setupViews() {
+        
+        let btnMenu1 = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(handelBackButton))
+        self.navigationItem.rightBarButtonItem = btnMenu1
+        
+        
+        let btnMenu = UIBarButtonItem(image: #imageLiteral(resourceName: "menu"), style: .plain, target: self, action: #selector(btnMenuAction) )
+        btnMenu.tintColor=UIColor(red: 54/255, green: 55/255, blue: 56/255, alpha: 1.0)
+        self.navigationItem.leftBarButtonItem = btnMenu
+        
+        sidebarView=SidebarView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.frame.height))
+        sidebarView.delegate=self
+        
+        sidebarView.layer.zPosition=100
+        self.view.isUserInteractionEnabled=true
+        self.navigationController?.view.addSubview(sidebarView)
+        
+        blackScreen=UIView(frame: self.view.bounds)
+        blackScreen.backgroundColor=UIColor(white: 0, alpha: 0.5)
+        blackScreen.isHidden=true
+        self.navigationController?.view.addSubview(blackScreen)
+        blackScreen.layer.zPosition=99
+        let tapGestRecognizer = UITapGestureRecognizer(target: self, action: #selector(blackScreenTapAction(sender:)))
+        blackScreen.addGestureRecognizer(tapGestRecognizer)
+    }
+    func setupWebView(url: URL) {
+        self.view.addSubview(myWebView)
+        let myURLRequest:URLRequest = URLRequest(url: url)
+        myWebView.delegate = self
+        myWebView.loadRequest(myURLRequest)
+    }
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        mainActivityIndicator.stopAnimating()
+    }
+    
+    func loading() {
+        mainActivityIndicator.center = self.view.center
+        mainActivityIndicator.hidesWhenStopped = true
+        mainActivityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.whiteLarge
+        mainActivityIndicator.color = UIColor.darkGray
+        mainActivityIndicator.startAnimating()
+        self.view.addSubview(mainActivityIndicator)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = URL(string:  "http://m.paylesstravel.com.au/")
+        setupWebView(url: myURL!)
+        setupWebApp()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupWebApp() {
+        loading()
+        setupViews()
     }
-
-
+    
+    
+    @objc func handelBackButton() {
+        
+        if myWebView.canGoBack {
+            myWebView.goBack()
+        }
+    }
+    
+    
+    @objc func btnMenuAction() {
+        blackScreen.isHidden = false
+        UIView.animate(withDuration: 0.3, animations: {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 250, height: self.sidebarView.frame.height)
+        }) { (complete) in
+            self.blackScreen.frame=CGRect(x: self.sidebarView.frame.width, y: 0, width: self.view.frame.width-self.sidebarView.frame.width, height: self.view.bounds.height+100)
+        }
+    }
+    
+    @objc func blackScreenTapAction(sender: UITapGestureRecognizer) {
+        blackScreen.isHidden=true
+        blackScreen.frame=self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
+    }
+    
 }
 
+extension ViewController: SidebarViewDelegate {
+    func sidebarDidSelectRow(row: Row) {
+        blackScreen.isHidden=true
+        blackScreen.frame=self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
+        switch row {
+        case .editProfile:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .home:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .flights:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .hotels:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/ean/ean2")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .cars:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/cartrawler/search_car")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .insurance:
+            let myURL = URL(string:  "http://m.paylesstravel.com.au/Insurance")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .blog:
+            let myURL = URL(string:  "http://m.paylesstravel.com.au/blog")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .about_us:
+            let myURL = URL(string:  "http://m.paylesstravel.com.au/About-US")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .contact_us:
+            let myURL = URL(string: "http://m.paylesstravel.com.au/Contact-US")
+            setupWebView(url: myURL!)
+            setupWebApp()
+        case .none:
+            break
+            //        default:  //Default will never be executed
+            //            break
+        }
+    }
+    
+    
+}
